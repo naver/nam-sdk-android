728b8b967b47328c160dea9b755cf8b26b6c5787
yeojun-yoon <yjyoon.dev@navercorp.com>
2025-03-14 17:28:53 +0900

    chore: migrate gradle to Kotlin DSL

---
+++ build.gradle.kts
@@ -0,0 +1,49 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+        plugins {
+            id("com.android.application") version "8.8.0" apply false
+            id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+            id("com.diffplug.spotless") version "6.7.2" apply true
+        }
+
+        // for the convention. you don't need to use this
+        spotless {
+            format("misc") {
+                target("*.gradle", "*.md", ".gitignore")
+                trimTrailingWhitespace()
+                indentWithSpaces()
+                endWithNewline()
+            }
+            java {
+                target("**/*.java")
+                removeUnusedImports()
+                licenseHeader(
+                    """
+                    /*
+                     * NAM(Naver Ad Manager) SDK for Android
+                     * 
+                     * Copyright 2022-present NAVER Corp.
+                     * All rights reserved.
+                     * 
+                     * Unauthorized use, modification and redistribution of this software are strongly prohibited.
+                     */
+                    """.trimIndent()
+                )
+                googleJavaFormat().aosp()
+            }
+            kotlin {
+                target("**/*.kt")
+                licenseHeader(
+                    """
+                    /*
+                     * NAM(Naver Ad Manager) SDK for Android
+                     * 
+                     * Copyright 2022-present NAVER Corp.
+                     * All rights reserved.
+                     * 
+                     * Unauthorized use, modification and redistribution of this software are strongly prohibited.
+                     */
+                    """.trimIndent()
+                )
+                ktlint()
+            }
+        }
---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://android-sdk.is.com/") }
+        maven { url = uri("https://artifactory.bidmachine.io/bidmachine") }
+        maven { url = uri("https://cboost.jfrog.io/artifactory/chartboost-ads/") }
+    }
+}
+rootProject.name = "NamExample"
+include(":java-sample")
+include(":kotlin-sample")
